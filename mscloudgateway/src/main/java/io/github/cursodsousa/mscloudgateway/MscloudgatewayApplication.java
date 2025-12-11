a4e768b2ec000747ab3bde7d5df0a44a009376bc
JulianaMichelsen <juju.michelsen@gmail.com>
2025-12-11 16:50:00 -0300

    feat: Cria as rotas para os microservicos

--- mscloudgateway/src/main/java/io/github/cursodsousa/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/cursodsousa/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,23 @@ package io.github.cursodsousa.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+					.route(r -> r.path("/clientes/**").uri("ld://msclientes"))
+				.build();
 	}
 
 }
