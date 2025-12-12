b29cf2de15e972f7e8781c73f01284b5694a295b
JulianaMichelsen <juju.michelsen@gmail.com>
2025-12-12 07:53:10 -0300

    fix: corrige application

--- msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClientesController.java
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/application/ClientesController.java
@@ -4,6 +4,7 @@ import io.github.cursodsousa.msclientes.application.representation.ClienteSaveRe
 import io.github.cursodsousa.msclientes.domain.Cliente;
 import jakarta.servlet.Servlet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,12 +14,14 @@ import java.net.URI;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientesController {
 
     private final ClienteService service;
 
     @GetMapping
     public  String status(){
+        log.info("Obtendo o status do microservice de clientes");
         return "ok";
     }
 
